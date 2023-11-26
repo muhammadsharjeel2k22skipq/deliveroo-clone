92cceaa78ebd658a0f62f8f67fc93270867525ae
Muhammad Sharjeel <muhammadsharjeel@Muhammads-MacBook-Pro.local>
2023-11-26 14:06:41 +0500

    Added new screen

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Categories } from './Categories.jsx';
 export { default as CategoryCard } from './CategoryCard.jsx';
 export { default as FeaturedRow } from './FeaturedRow.jsx';
 export { default as RestaurantCard } from './RestaurantCard.jsx';
+export { default as DishRow } from './DishRow.jsx';
